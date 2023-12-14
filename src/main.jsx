5362fa30d9017dfecf900bfbe069aad4a6031f14
MEGHAJANGWAN <jangwanmegha@gmail.com>
2023-12-14 14:14:04 +0530

    Added react-hot-toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-// Component imports
 // CSS imports
 import './index.css';
 
 // Library imports
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
+// Component imports
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
        <App />
+       <Toaster />
     </BrowserRouter>
 );
