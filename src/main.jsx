9ffd6c75442f9163e0d7778ce763b660019ff88d
MEGHAJANGWAN <jangwanmegha@gmail.com>
2023-12-14 14:52:35 +0530

    setup of axios instance done

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider>
+    <Provider store= { store }>
     <BrowserRouter>
        <App />
        <Toaster />
