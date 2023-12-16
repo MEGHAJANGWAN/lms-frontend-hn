282c726650f584b25236d9e49e407f02b77363ae
MEGHAJANGWAN <jangwanmegha@gmail.com>
2023-12-16 17:21:53 +0530

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes} from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
+import NotFound from './pages/NotFound';
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/about" element={ <AboutUs/> }></Route>
+        <Route path="/about" element={ <AboutUs />}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
       
     </>
