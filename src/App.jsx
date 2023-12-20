890f697c9dbd187c4851f9db6ec07b01cdae257b
MEGHAJANGWAN <jangwanmegha@gmail.com>
2023-12-20 12:05:47 +0530

    Completed course list page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Routes} from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
@@ -17,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={ <AboutUs />}></Route>
+        <Route path="/courses" element={ <CourseList/> }></Route>
+
       
         <Route path="/signup" element={ <Signup/> }></Route>
         <Route path="/login" element={ <Login/> }></Route>
--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -1,10 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+import axiosInstance from "../../Helpers/axiosInstance";
 
 const initialState  = {
     courseData: []
 }
 
-export const getAllCourses = createAsyncThunk("/course/get", async () =>{
+export const getAllCourses = createAsyncThunk("/course/get", async () => {
     try {
         const response = axiosInstance.get("/courses");
         toast.promise(response, {
@@ -13,9 +16,9 @@ export const getAllCourses = createAsyncThunk("/course/get", async () =>{
             error: "Failed to get the courses",
         });
 
-        return (await response).data.courses,
+        return (await response).data.courses;
 
-    } catch (error) {
+        } catch (error) {
         toast.error(error?.response?.data?.message);
     }
 });
@@ -25,6 +28,11 @@ const courseSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder) => {
+        builder.addCase(getAllCourses.fulfilled, (state, action) => {
+            if(action.payload){
+                state.courseData = [...action.payload];
+            }
+        })
 
     }
 });
