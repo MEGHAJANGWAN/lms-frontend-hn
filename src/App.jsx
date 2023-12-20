e3c45b5c710eb2480159273b78c8eb0878c71ef3
MEGHAJANGWAN <jangwanmegha@gmail.com>
2023-12-21 00:02:32 +0530

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Route, Routes} from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import Contact from './Pages/Contact';
 import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
@@ -19,6 +20,8 @@ function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={ <AboutUs />}></Route>
         <Route path="/courses" element={ <CourseList/> }></Route>
+        <Route path="/contact" element={ <Contact/> }></Route>
+
 
       
         <Route path="/signup" element={ <Signup/> }></Route>
